b7068295148ff6338d0584c55c630ccfd2b0f298
Robert Kaussow <mail@thegeeklab.de>
2025-02-20 20:57:06 +0100

    feat: add environment option

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestFlags(t *testing.T) {
+func TestSecretsFlag(t *testing.T) {
 	tests := []struct {
 		name string
 		envs map[string]string
@@ -36,6 +36,40 @@ func TestFlags(t *testing.T) {
 
 		if !reflect.DeepEqual(got.Settings.Build.Secrets, tt.want) {
 			t.Errorf("%q. Build.Secrets = %v, want %v", tt.name, got.Settings.Build.Secrets, tt.want)
+		}
+	}
+}
+
+func TestEnvironmentFlag(t *testing.T) {
+	tests := []struct {
+		name string
+		envs map[string]string
+		want []string
+	}{
+		{
+			name: "parse secrets list with escape",
+			envs: map[string]string{
+				"PLUGIN_ENVIRONMENT": `{"env1": "value1", "env2": "value2"}`,
+			},
+			want: []string{
+				"env1=value1",
+				"env2=value2",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		for key, value := range tt.envs {
+			t.Setenv(key, value)
+		}
+
+		got := New(func(_ context.Context) error { return nil })
+
+		_ = got.App.Run([]string{"wp-docker-buildx"})
+		_ = got.FlagsFromContext()
+
+		if !reflect.DeepEqual(got.Plugin.Environment.Value(), tt.want) {
+			t.Errorf("%q. Plugin.Environment = %v, want %v", tt.name, got.Plugin.Environment.Value(), tt.want)
 		}
 	}
 }
