350e7fa5878638d54c186d873ad661067f0eaf54
Robert Kaussow <mail@thegeeklab.de>
2025-04-08 11:19:38 +0200

    chore: add integration tests for StringSliceFlag StringMapFlag

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -74,3 +74,86 @@ func TestEnvironmentFlag(t *testing.T) {
 		})
 	}
 }
+
+func TestCacheFromFlag(t *testing.T) {
+	tests := []struct {
+		name string
+		envs map[string]string
+		want []string
+	}{
+		{
+			name: "simple escape",
+			envs: map[string]string{
+				"PLUGIN_CACHE_FROM": `type=registry\,ref=example,foo=bar`,
+			},
+			want: []string{
+				"type=registry,ref=example",
+				"foo=bar",
+			},
+		},
+		{
+			name: "double escape",
+			envs: map[string]string{
+				"PLUGIN_CACHE_FROM": "type=registry\\,ref=example,foo=bar",
+			},
+			want: []string{
+				"type=registry,ref=example",
+				"foo=bar",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for key, value := range tt.envs {
+				t.Setenv(key, value)
+			}
+
+			got := New(func(_ context.Context) error { return nil })
+
+			_ = got.App.Run([]string{"wp-docker-buildx"})
+			_ = got.FlagsFromContext()
+
+			assert.ElementsMatch(t, tt.want, got.Settings.Build.CacheFrom)
+		})
+	}
+}
+
+func TestBuildArgsFlag(t *testing.T) {
+	tests := []struct {
+		name string
+		envs map[string]string
+		want map[string]string
+	}{
+		{
+			name: "not nil",
+			envs: map[string]string{},
+			want: map[string]string{},
+		},
+		{
+			name: "parse args",
+			envs: map[string]string{
+				"PLUGIN_BUILD_ARGS": `{"arg1": "value1", "arg2": "value2"}`,
+			},
+			want: map[string]string{
+				"arg1": "value1",
+				"arg2": "value2",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for key, value := range tt.envs {
+				t.Setenv(key, value)
+			}
+
+			got := New(func(_ context.Context) error { return nil })
+
+			_ = got.App.Run([]string{"wp-docker-buildx"})
+			_ = got.FlagsFromContext()
+
+			assert.Equal(t, tt.want, got.Settings.Build.Args)
+		})
+	}
+}
